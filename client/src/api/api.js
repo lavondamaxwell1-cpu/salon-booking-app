c52f7a1e91853eae5b1b9443e27f5a55bc060900
Lavonda Maxwell <lavondamaxwell1@gmail.com>
2026-05-08 22:10:06 -0400

    Fix frontend API base URL

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://salon-booking-app-p4xz.onrender.com",
+  baseURL: "https://salon-booking-app-p4xz.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
--- client/src/api/socket.js
+++ client/src/api/socket.js
@@ -1,5 +1,4 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
-
+const socket = io("https://salon-booking-app-p4xz.onrender.com");
 export default socket;
