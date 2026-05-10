90098f1072c75a862b5ea35534c615369cd789e0
Lavonda Maxwell <lavondamaxwell1@gmail.com>
2026-05-10 08:20:12 -0400

    fixing the server

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import http from "http";
 import { Server } from "socket.io";
 
 import { connectDB } from "./config/db.js";
+
 import adminRoutes from "./routes/adminRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import stylistRoutes from "./routes/stylistRoutes.js";
@@ -13,24 +14,39 @@ import userRoutes from "./routes/userRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import reviewRoutes from "./routes/reviewRoutes.js";
 import paymentRoutes from "./routes/paymentRoutes.js";
+
 import { stripeWebhook } from "./controllers/paymentController.js";
 
 import "./jobs/appointmentReminderJob.js";
 
 dotenv.config();
+
 connectDB();
 
 const app = express();
 const server = http.createServer(app);
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://salon-booking-app-pi-lake.vercel.app",
+];
+
+// SOCKET.IO
 export const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:5173",
-      "https://salon-booking-app-pi-lake.vercel.app",
-      "https://salon-booking-olzcftqef-lavondamaxwell1-cpus-projects.vercel.app",
-    ],
+    origin: (origin, callback) => {
+      if (
+        !origin ||
+        allowedOrigins.includes(origin) ||
+        origin.includes("vercel.app")
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   },
 });
 
@@ -42,29 +58,41 @@ io.on("connection", (socket) => {
   });
 });
 
+// EXPRESS CORS
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://salon-booking-app-pi-lake.vercel.app",
-      "https://salon-booking-olzcftqef-lavondamaxwell1-cpus-projects.vercel.app",
-    ],
+    origin: (origin, callback) => {
+      if (
+        !origin ||
+        allowedOrigins.includes(origin) ||
+        origin.includes("vercel.app")
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
 );
+
+// STRIPE WEBHOOK
 app.post(
   "/api/payments/webhook",
   express.raw({ type: "application/json" }),
   stripeWebhook,
 );
 
+// BODY PARSER
 app.use(express.json());
 
+// TEST ROUTE
 app.get("/", (req, res) => {
   res.send("Salon Booking API is running");
 });
 
+// ROUTES
 app.use("/api/auth", authRoutes);
 app.use("/api/stylists", stylistRoutes);
 app.use("/api/appointments", appointmentRoutes);
@@ -74,6 +102,7 @@ app.use("/api/reviews", reviewRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/admin", adminRoutes);
 
+// SERVER
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
